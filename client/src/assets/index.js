744ba5f276e8287f626707196e5265cecaedc5d1
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-30 12:51:55 +0100

    README.md modified

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,7 +2,6 @@ import ai from "./ai.png";
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
 import download from "./download.png";
-
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
