524082401275fb7fb9c914f77c32f3668a7a855f
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-01 17:37:48 +0100

    env added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dalleRoutes from './routes/dalle.routes.js'
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors())
 
 app.use(express.json({limit: "50mb"}))
 
