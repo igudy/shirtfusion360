013e723a93a832d0034f24667998495abb4e8d4c
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-01 12:30:45 +0100

    Working on server side

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -41,9 +41,29 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />
       case "aipicker":
-        return <AIPicker />
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        )
       default:
         return null
+    }
+  }
+
+  const handleSubmit = () => {
+    if (!prompt) return alert("Please entr a prompt")
+
+    try {
+      // call our backend to generate an ai image.
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false)
+      setActiveEditorTab("")
     }
   }
 
