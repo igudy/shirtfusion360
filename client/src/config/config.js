49da7076501e58ef8825df699c326fd4309a6d7b
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-02 16:04:11 +0100

    Deployment fixes

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,10 @@
 const config = {
   development: {
+    // port 8080 doesn't work in nigeria, use other port except port 5000
     backendUrl: "http://localhost:5000/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "",
   },
 };
 
